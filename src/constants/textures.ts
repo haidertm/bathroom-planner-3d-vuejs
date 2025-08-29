1eed93d202363116eabcc0778948621eeaed72b4
hamza-afridi <hamza.khan@ki5.co.uk>
2025-08-29 11:49:16 +0500

    fix: default textures changed

--- src/constants/textures.ts
+++ src/constants/textures.ts
@@ -28,8 +28,8 @@ export const WALL_TEXTURES: readonly TextureConfig[] = [
   { name: 'Subway Tile', file: '', color: 0xf0f0f0, scale: [8, 6] }
 ];
 
-export const DEFAULT_FLOOR_TEXTURE: number = 6; // Wood
-export const DEFAULT_WALL_TEXTURE: number = 0; // Brick
+export const DEFAULT_FLOOR_TEXTURE: number = 0; // white
+export const DEFAULT_WALL_TEXTURE: number = 6; // green
 
 
 // Enhanced lighting constants
